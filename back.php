1b85cf1ad179eff1071c5f535867fe243965ab4d
RonSamuel <109104664+RonSamuel@users.noreply.github.com>
2023-09-28 16:37:19 -0400

    Update back.php

--- back.php
+++ back.php
@@ -1,5 +1,23 @@
-<html>
-<body>
-   <?php echo '<p>Hello, World ---- Online</p>'; ?>  
-</body>
-</html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$db = "sakila";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT film.title,film.film_id,film.release_year, film.length from film join inventory on film.film_id = inventory.film_id join rental on rental.inventory_id = inventory.inventory_id group by film.title order by count(*) DESC LIMIT 5;";
+$result = mysqli_query($conn, $sql);
+
+while($row = mysqli_fetch_assoc($result)){
+    $json_data[] = $row;
+}
+echo json_encode($json_data);
+?>
