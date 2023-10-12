97603d0eb487c5bf3b513253bfd6d88841dc6777
RonSamuel <109104664+RonSamuel@users.noreply.github.com>
2023-10-12 10:26:31 -0400

    fix $conn t5actor.php

--- t5actor.php
+++ t5actor.php
@@ -1,17 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "1234";
-$db = "sakila";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $db);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+include ('databaseInfo.php');
+/**
+ * @var databaseInfo $conn
+ */
 
 $sql = "SELECT actor.first_name, actor.last_name, actor.actor_id, count(*) AS total from actor join film_actor on actor.actor_id = film_actor.actor_id group by actor.actor_id order by count(*) DESC LIMIT 5;";
 $result = mysqli_query($conn, $sql);
