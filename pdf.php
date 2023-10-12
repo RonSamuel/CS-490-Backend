a28f4688fd2c85566c145ee7fb814adc85ad239a
RonSamuel <109104664+RonSamuel@users.noreply.github.com>
2023-10-12 10:26:04 -0400

    fix $conn pdf.php

--- pdf.php
+++ pdf.php
@@ -1,18 +1,9 @@
 <?php
 require_once 'FPDF/fpdf.php';
-
-$servername = "localhost";
-$username = "root";
-$password = "1234";
-$db = "sakila";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $db);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include ('databaseInfo.php');
+/**
+ * @var databaseInfo $conn
+ */
 
 $sql = "SELECT * from customer;";
 $result = mysqli_query($conn, $sql);
